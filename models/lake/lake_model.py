7b9f276afdaf23c6b0540e0b54edab09b3f89d3c
Luciano Raso <luciofaso@gmail.com>
2018-09-18 22:41:00 +0200

    include forcings

--- models/lake/lake_model.py
+++ models/lake/lake_model.py
@@ -23,30 +23,36 @@ def sim_H(h_init, forcings, par_lake, par_wind, policy):
 
 
     # preprocess input
-    forcings['wind velocity'][forcings['wind velocity']<0] = 0 # set negative wind velocity to zero
+    #forcings['wind velocity'][forcings['wind velocity']<0] = 0 # set negative wind velocity to zero
 
     daily_variables = forcings.columns.drop('sea level')
 
-    forcings_daily = forcings[daily_variables].resample('D')
+    forcings_daily = forcings[daily_variables.values].resample('D').mean()
 
 
+    S_lat = 1.419 * 1000000  # km^2 to m^2
+    alpha = 0.5
+    forcings_daily['inflow lateral'] = forcings_daily['precipitation'] * S_lat * alpha / par_lake['Delta_t']  # rational formula
 
     # forcings
-    q_ij = forcings_daily['discharge Ijssel'].values
-    q_lat = forcings_daily['discharge lateral'].values
+    q_ij = forcings_daily['inflow ijssel'].values
+    q_lat = forcings_daily['inflow lateral'].values
     temperature = forcings_daily['temperature'].values
-
-    wind_v_mean = forcings['wind velocity, average'].values
-    wind_v_max = forcings['wind velocity, max'].values
-    wind_d = forcings['wind direction'].values
+    wind_v_mean = forcings_daily['wind speed, average'].values
+    wind_d = forcings_daily['wind direction'].values
+    pot_evaporation = forcings_daily['potential evaporation'].values
 
     n_days = len(q_ij)
-    h_sea = forcings['sea level, max'].values[:n_days*24].reshape((n_days,24))
+    h_sea = forcings['sea level'].values[:n_days*24].reshape((n_days,24))
     # TODO: test that input arrays have no nan cells
 
+
+
     # policy options
-    h_target_t = policy['target level']
+    h_target = set_target_level(policy['winter target'], policy['summer target'], forcings_daily.index).values
+
     pump_capacity = policy['pumping capacity']
+    E_pump = policy['pump power']
 
     # lake parameters
     Delta_t = par_lake['Delta_t']
@@ -78,14 +84,14 @@ def sim_H(h_init, forcings, par_lake, par_wind, policy):
     for t in range(1,H-1):
         h_wind[t] = wind_setup (wind_v_mean[t], wind_d[t], h_bar[t-1], a, b, c, h_0_wind)
         h_bar[t],q_free[t], q_pump[t],q_demand[t], q_supply[t] = lake_sim_step (h_bar[t-1], h_wind[t],
-                                                  q_ij[t], q_lat[t], temperature[t],
+                                                  q_ij[t], q_lat[t], temperature[t], pot_evaporation[t],
                                                   h_sea[t][:],
-                                                  h_target_t, pump_capacity,
+                                                  h_target[t], E_pump, pump_capacity,
                                                   Delta_t/A, K, k_evap, k_dem)
 
 
-    output = pd.DataFrame(np.array([h_bar, h_wind, q_out, q_demand, q_supply]).transpose(),
-                          index = forcings.index, columns=['h_bar', 'h_wind', 'q_out', 'q_demand', 'q_supply'])
+    output = pd.DataFrame(np.array([h_bar, h_wind, q_free,q_pump, q_demand, q_supply]).transpose(),
+                          index = forcings_daily.index, columns=['h_bar', 'h_wind', 'q_free','q_pump', 'q_demand', 'q_supply'])
 
     return (output)
 
@@ -109,10 +115,33 @@ def wind_setup (wind_velocity, wind_direction, h_bar, a, b, c, h_0):
     return h_wind
 
 
+def set_target_level(winter_target, summer_target, time_index):
+    """
+
+    Args:
+        winter_target: target water level on the lake, winter period, i.e. from October to March
+        summer_target: target water level on the lake, summer period, i.e. from April to September
+        time_index: time index for which the series of target level must be set
+
+    Returns:
+        target_level (pd.Series): series of target water level
+
+    """
+    april = 3
+    september = 8
+
+    target_level = pd.Series(index=time_index)
+    target_level[:] = winter_target
+    target_level[np.logical_and(target_level.index.month >= april , target_level.index.month <= september)] = summer_target
+
+    return target_level
+
+
+
 def lake_sim_step(h_bar_tmin1, h_wind_t,
-                  q_ij_t, q_lat_t, temperature_t,
-                  h_sea_t: np.array,
-                  h_target_t, pump_capacity,
+                  q_ij_t, q_lat_t, temperature_t,pot_evaporation_t,
+                  h_sea_hourly: np.array,
+                  h_target_t, E_pump, pump_capacity,
                   Delta_t_S, K, k_evap, k_dem
                   ):
     """Dynamic model of the lake
@@ -136,43 +165,45 @@ def lake_sim_step(h_bar_tmin1, h_wind_t,
         h_bar_t, q_free, q_pump, q_demand_t, q_supply_t
     """
 
-    
     # fluxes
-    q_free, q_pump_max = discharge_afsluitdijk(h_bar_tmin1 + h_wind_t, h_sea)# np.sqrt(2 * 9.8 * max(h_bar_tmin1 + h_wind_t - h_wz_t, 0))
-    q_evaporation_t = k_evap * temperature_t
+    q_free_max, q_pump_max = discharge_afsluitdijk(h_bar_tmin1 + h_wind_t - h_sea_hourly, K, E_pump, pump_capacity)# np.sqrt(2 * 9.8 * max(h_bar_tmin1 + h_wind_t - h_wz_t, 0))
     q_demand_t = k_dem * temperature_t # oversimplified function of demand, proportional to temperature
     q_supply_t = q_demand_t # heroic assumption, supply always satisfied TODO set low level lake limit
 
     #levels
-    Delta_h_bar_no_pump = Delta_t_S * (q_ij_t + q_lat_t - q_free - q_supply_t - q_evaporation_t)
-    h_bar_t = h_bar_tmin1 + Delta_h_bar_no_pump
-    if h_bar_t >= h_target_t:
-        q_pump = np.min( Delta_t_S * (h_bar_t - h_target_t) , q_pump_max)
-        h_bar_t = h_bar_t - Delta_t_S * q_pump
+    Delta_h_bar_no_pump_no_free = Delta_t_S * (q_ij_t + q_lat_t - q_supply_t ) - pot_evaporation_t
+    h_bar_t = h_bar_tmin1 + Delta_h_bar_no_pump_no_free
+
+    # spui als het kan
+    q_free = np.minimum(  (h_bar_t - h_target_t) / Delta_t_S, q_free_max) if h_bar_t >= h_target_t else 0
+    h_bar_t = h_bar_t - Delta_t_S * q_free
+    #pump als het moet
+    q_pump = np.minimum(  (h_bar_t - h_target_t) / Delta_t_S, q_pump_max) if h_bar_t >= h_target_t else 0
+    h_bar_t = h_bar_t - Delta_t_S * q_pump
 
     return h_bar_t, q_free, q_pump, q_demand_t, q_supply_t
 
 
 
-def discharge_afsluitdijk(h_lake: float, h_sea: np.array, K: float, E: float):
-    """
+def discharge_afsluitdijk(Delta_h: np.array, K: float, E_pump: float,pump_capacity:float):
+    """Return the maximum free discharge and pumping discharge for given water levels
 
     Args:
-        h_lake (float): water level in the lake, i.e. average water level + wind setup at sluices, in mNAP
-        h_sea (24 hours array): hourly sea water level, in mNAP
+        Delta_h (np.array): Difference between
+            * daily water level in the lake, i.e. average water level + wind setup at sluices, in mNAP
+            * hourly sea water level, in mNAP
         K (float): sluices carachteristics, see (Talsma), in m^2
         E (float): pumping power, in [XX]
 
     Returns:
-        q_free : free discharge through the sluices, in m^3/s
+        q_free : max free discharge through the sluices, in m^3/s
         q_pump_max: max pump, in m^3/s
     """
 
-    Delta_h = h_lake - h_sea
-    q_free = np.mean( K * np.sqrt(2 * 9.8 * np.max(Delta_h,0) ) )
-    q_pump_max = np.mean( E /  np.max(-Delta_h,0) )
+    q_free_max = np.mean( K * np.sqrt(2 * 9.8 * np.maximum(Delta_h,0) ) )
+    q_pump_max = np.mean( np.minimum( E_pump / np.maximum(-Delta_h,0),pump_capacity) )
 
-    return (q_free, q_pump_max)
+    return q_free_max, q_pump_max
 
 
 
@@ -196,7 +227,7 @@ def yearly_max_wl(h_bar, forcings, wind_par):
     h_bar_mean = h_df.mean(axis=0).values
     #wind = forcings[['wind velocity','wind direction']]
 
-    h_df['h_wind'] = wind_setup(forcings['wind velocity, max'].values, forcings['wind direction'].values, h_bar_mean, wind_par['a'], wind_par['b'], c, wind_par['h_0'])
+    h_df['h_wind'] = wind_setup(forcings['wind speed, hourly max'].values, forcings['wind direction'].values, h_bar_mean[0], wind_par['a'], wind_par['b'], c, wind_par['h_0'])
     h_df['h_t'] = h_df['h_bar'] + h_df['h_wind']
 
     h_year_max = h_df['h_t'].groupby(h_df.index.year).max()
@@ -205,61 +236,3 @@ def yearly_max_wl(h_bar, forcings, wind_par):
 
 
 
-
-
-
-
-# TODO: TO BE DELETED
-
-def free_discharge(Delta_h_lake, var_h_sea, Delta_t=24*3600):
-    """Calculate the free discharge from the sluices
-
-    Args:
-        Delta_h_lake (float): difference between water level in the lake, sum of average lake level and wind setup at
-            sluices  (in m NAP) and daily average of sea water level (in m NAP)
-        var_h_sea (float): difference between the maximum and the minimum sea water level (in m)
-        integral_free_discharge_AfD (function): function calculating the integral of the sqrt ( sin (t) - diff )
-        Delta_t (float)=: time-step lenght (in seconds, default: one day)
-    Returns:
-        q_free (float): the discharge (in $m^3/s$)
-    """
-
-    pos = Delta_h_lake / (var_h_sea / 2)
-
-    if pos >= 1:  # i.e. h_lake - shift_h_sea < delta_h_sea/2:
-        t_start = np.arcsin(-pos)
-        t_end = np.arcsin(pos)
-        q_free = integral_free_discharge(t_start, Delta_h_lake, var_h_sea, Delta_t) \
-                 - integral_free_discharge(t_end, Delta_h_lake, var_h_sea, Delta_t)
-    else:
-        q_free = 0
-
-    return (q_free)
-
-
-
-def integral_free_discharge(t, delta_h_lake, delta_h_sea, Delta_t, freq_sin=24.5*3600):
-    """Calculate comulative discharge
-
-    Args:
-        t: instant of integral
-        h_lake: water level in the lake, sum of average and wind setup level
-        shift_h_sea:
-        delta_h_sea:
-        K:
-        Delta_t:
-        freq_sin: frequency of tide
-
-    Returns:
-
-    """
-
-    a = delta_h_sea / 2
-    b = delta_h_lake
-    c = freq_sin / (2 * np.pi)
-    k1 = np.sqrt(b-a*np.sin(c*t))
-    phi = (np.pi - 2 * c * t) / 4
-    m = a / ( (a-b) / 2 )
-
-    cum_discharge = 2 * np.sqrt( 9.8 np.abs(a-b)) * k1 * ellipeinc(phi, m)
-    return (cum_discharge)
